6ae07034c79a29ef618a5162a2231662e1584a47
Your Name <isaa git config --global user.email you@example.com>
2021-11-17 07:07:35 +0200

    added cart visibility state

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
+import { useState } from 'react';
 import Header from './components/Layout/Header';
 import Meals from './components/Meals/Meals';
 import CartProvider from './store/CartProvider';
 
 function App() {
+
+ const [cartIsShown, setCartIsShown] = useState(false);
+
+ function showCardHandler() {
+  setCartIsShown(true);
+ }
+
+ function hideCardHandler() {
+  setCartIsShown(false);
+ }
+
   return (
     <CartProvider>
-      <Header />
+      <div className="find_wrapper">
+      {cartIsShown && <div className="find"> Card...</div>}
+      </div>
+      <Header onShowCart={showCardHandler}/>
       <main>
         <Meals />
       </main>
--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -7,6 +7,7 @@ import classes from './Header.module.css';
 const Header = (props) => {
   return (
     <Fragment>
+      
       <header className={classes.header}>
         <h1>ReactMeals</h1>
         <HeaderCartButton onClick={props.onShowCart} />
